7912c2e174c5f8056a50e0308555ce5bd2f0382c
“[TantuniKemal]” <[oztk.erkn@gmail.com]>
2024-08-14 22:01:50 +0300

    deva

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,3 +1,4 @@
+
 import { useState } from "react";
 import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
 import { icons } from "../constants";
@@ -16,24 +17,27 @@ const FormFiled = ({
             <View className={`space-y-2 ${otherStyles}`}>
                 <Text classname = "text-base text-gray-100 font-pmedium">{title}</Text>
                 <View classname = "w-full h-16 px-4 bg-black-100 rounded-2xl border-black-200 focus:border-secondary flex flex-row items-center">
+                    <View className="flex-row justify-center" >
                     <TextInput
-                        classname = "flex*2 text-white font-psemibold text-base"
+                        classname = "flex-1 text-white font-psemibold text-base"
                         value = {value}
                         placeholder = {placeholder}
-                        placeholderTextColor = "gray-200"
+                        placeholderTextColor = "#7B7B8B"
                         onchangeText = {handleChangeText}
                         secureTextEntry = {title === 'Şifre' && !showPassword}
                         {...props} 
                     />
                     {title === 'Şifre' && (
-                        <TouchableOpacity onPress = {() => setShowPassword(!showPassword)}>
+                        <TouchableOpacity
+                        onPress = {() => setShowPassword(!showPassword)}>
                             <Image
                                 source ={!showPassword ? icons.eye : icons.eyeHide}
-                                classname = "w-6 h-6"
+                                style={{width:30, height:30}}
                                 resizeMode = "contain"
                             />
                         </TouchableOpacity>
                     )}
+                    </View>
                 </View>
             </View>
         );
