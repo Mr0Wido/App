d80b3d53340ba5c6dfdbe94ad4ede8144052ccce
“[TantuniKemal]” <[oztk.erkn@gmail.com]>
2024-08-15 20:39:59 +0300

    nt

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -19,7 +19,7 @@ const FormField = ({
         <View className={`space-y-2 ${otherStyles}`}>
             <Text className="text-base text-newTextColor font-pmedium">{title}</Text>
 
-            <View className="w-80 h-10 px-12 bg-white rounded-2xl border-2 border-gray-200 focus:border-secondary  items-center flex-row"
+            <View className="w-80 h-10 px-12 bg-white rounded-2xl border-2 border-gray-200 focus:border-secondary  justify-center items-center flex-row"
            >
              {icon && (
             <Image
@@ -38,21 +38,25 @@ const FormField = ({
                 onChangeText={handleChangeText}
                 source={icon}
                 secureTextEntry={title === "Şifre" && !showPassword}
-                {...props}
+               
             />
-
+            
+            
+            </View>
             {title === "Şifre" && (
+                <View className="flex-row">
                 <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
                 <Image
                     source={!showPassword ? icons.eye : icons.eyeHide}
-                    className="w-6 h-6 flex-row"
+                    className="w-6 h-6"
                     resizeMode="contain"
+                    
                 />
                 </TouchableOpacity>
+                </View>
            
             )}
             
-            </View>
         </View>
     );
 };
