ad16dd20f400bb6ff42afa49f5c6b2651f4e7198
Furkan Deniz <mr0wido@protonmail.com>
2024-08-18 15:21:55 +0300

    new

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,16 @@
-// babel.config.js
 module.exports = function (api) {
   api.cache(true);
   return {
+    presets: [
+        [ "@babel/preset-env",
+          {
+            "targets": {
+              "node": "current"
+            }
+          }
+        ],
+        "@babel/preset-typescript"
+      ],
     presets: ["babel-preset-expo"],
     plugins: ["nativewind/babel"],
   };
