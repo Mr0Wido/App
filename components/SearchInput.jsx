1493975456139b1891096964a6d0a0f574092d69
“[TantuniKemal]” <[oztk.erkn@gmail.com]>
2024-09-03 22:31:08 +0300

    aa

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -1,52 +1,34 @@
-
-import { View, Text, TextInput, Image, Pressable, } from "react-native";
-import { useState } from 'react';
-
+import { View, Text, TextInput, Image, Pressable } from "react-native";
+import { useState } from "react";
 
 import { icons } from "../constants";
-const SearchInput= ({
-    value,
-    placeholder,
-    handleChangeText,
-    otherStyles,
-    ...props
-    }) => {
-     const handleIconPress = () => {
-        seticonPressed(prevState => !prevState)
-    }
-    const [iconPressed, seticonPressed] = useState(false)
-    return (
+const SearchInput = ({
+  value,
+  placeholder,
+  handleChangeText,
+  otherStyles,
+  ...props
+}) => {
+ 
+  return (
     <View className="w-full flex-row">
-        <View className="w-[85%] h-10 px-12 bg-slate-50 rounded-2xl border-2 border-gray-300 focus:border-secondary items-center flex-row"> 
-            <Image
-            source={icons.search}
-            className="w-5 h-5 right-8"
-            tintColor={'black'}
-            resizeMode='contain'
-            />  
-            <TextInput
-            className="text-base flex-1 text-black-100 text-psemibold"
-            value={value}
-            placeholder="Ürün ara"
-            placeholderTextColor="#7B7B8B"
-            onChangeText={handleChangeText}  
-            />
-        </View>
-     <View className="w-[15%] h-10 items-center justify-center">
-            <Pressable
-                onPress={handleIconPress}
-            >
-                <Image 
-                    source={icons.category}
-                    className="w-10 h-10"
-                    tintColor={iconPressed ? "orange" : "black"}
-                    resizeMode="contaion"
-                    
-                />
-            </Pressable>
-        </View>
-    </View>            
-    );
+      <View className="w-[85%] h-10 px-12 bg-slate-50 rounded-2xl border-2 border-gray-300 focus:border-secondary items-center flex-row">
+        <Image
+          source={icons.search}
+          className="w-5 h-5 right-8"
+          tintColor={"black"}
+          resizeMode="contain"
+        />
+        <TextInput
+          className="text-base flex-1 text-black-100 text-psemibold"
+          value={value}
+          placeholder="Ürün ara"
+          placeholderTextColor="#7B7B8B"
+          onChangeText={handleChangeText}
+        />
+      </View>
+    </View>
+  );
 };
 
 export default SearchInput;
