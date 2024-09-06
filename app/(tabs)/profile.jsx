c36d538b20adeb4944510bc229d218a26dd63374
“[TantuniKemal]” <[oztk.erkn@gmail.com]>
2024-09-06 06:38:33 +0300

    profileKeepGoing

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -23,11 +23,11 @@ const Profile = () => {
     companyAddress: "",
   });
   const [iconClicked, seticonClicked] = useState(false);
-  const [showForm, setshowForm] = useState(false);
+  const [showForms, setshowForms] = useState(false);
+  const [showOrders, setshowOrders] = useState(false);
 
   const handleIconPress = () => {
     seticonClicked((prevState) => !prevState);
-    
   };
 
   const handleActionPress = () => {
@@ -36,9 +36,12 @@ const Profile = () => {
     }
     submit();
   };
-  const handleShowForm = () => {
-    setshowForm((prevState) => !prevState);
-  }
+  const handleShowForms = () => {
+    setshowForms((prevState) => !prevState);
+  };
+  const handleShowOrders = () => {
+    setshowOrders((prevState) => !prevState);
+  };
 
   const submit = async () => {
     // Form submit logic
@@ -49,114 +52,124 @@ const Profile = () => {
       <ScrollView>
         <View className="w-full flex-col justify-center items-center mt-12">
           <View className="justify-center items-center flex-row flex-1">
-          <Image
+            <Image
               source={icons.user}
-              className="w-12 h-12 mr-8"
+              className="w-12  h-12 mr-8"
               style={{ tintColor: "orange" }} // statik tintColor
             />
-          <Text className="text-xl bg-slate-50 rounded-xl border-2 border-gray-300 text-black font-psemibold">
-          No: #5555
-          </Text>
+            <Text className="text-xl text-black font-psemibold">
+              Hesap Bilgileri
+            </Text>
           </View>
-          <CustomButton 
-            title={showForm ? "Bilgileri Gizle" : "Bilgileri Görüntüle"}
-            handlePress={handleShowForm}
+          <CustomButton
+            title={showForms ? "Bilgileri Gizle" : "Bilgileri Görüntüle"}
+            handlePress={handleShowForms}
             containerStyles="bg-primary w-64 h-12 mt-7 items-center"
-            icon={showForm ? icons.up : icons.down}
+            icon={showForms ? icons.up : icons.down}
           />
+          {showForms && (
+            <View className="w-full justify-center items-center mt-7">
+              <Text className="text-2xl font-psemibold">
+                Hesap Bilgileri {iconClicked ? "true" : "false"}
+              </Text>
+              <Pressable onPress={handleIconPress}>
+                <Image
+                  source={icons.change}
+                  className="w-12 h-12"
+                  style={{ tintColor: iconClicked ? "orange" : "black" }}
+                />
+              </Pressable>
+              <Text className=" mt-4 text-xl bg-slate-50 rounded-xl border-2 border-gray-300 text-black font-psemibold">
+                No: #5555
+              </Text>
+              <FormField
+                title="İsim"
+                value={form.username}
+                handleChangeText={(e) => setform({ ...form, username: e })}
+                isEditable={iconClicked}
+                icon={icons.heart}
+                otherStyles="mt-7"
+              />
+              <FormField
+                title="Soyisim"
+                value={form.surname}
+                handleChangeText={(e) => setform({ ...form, surname: e })}
+                isEditable={iconClicked}
+                icon={icons.surname}
+                otherStyles="mt-7"
+              />
+              <FormField
+                title="Email"
+                value={form.email}
+                handleChangeText={(e) => setform({ ...form, email: e })}
+                isEditable={iconClicked}
+                icon={icons.mail}
+                otherStyles="mt-7"
+              />
+              <FormField
+                title="Şifre"
+                value={form.password}
+                handleChangeText={(e) => setform({ ...form, password: e })}
+                isEditable={iconClicked}
+                icon={icons.password}
+                otherStyles="mt-7"
+              />
+              <FormField
+                title="Telefon Numarası"
+                value={form.phoneNumber}
+                handleChangeText={(e) => setform({ ...form, phoneNumber: e })}
+                isEditable={!iconClicked}
+                icon={icons.smartPhone}
+                otherStyles="mt-7"
+              />
+              <FormField
+                title="Şirket İsmi"
+                value={form.companyName}
+                handleChangeText={(e) => setform({ ...form, companyName: e })}
+                isEditable={iconClicked}
+                icon={icons.home}
+                otherStyles="mt-7"
+              />
+              <FormField
+                title="Adres"
+                value={form.companyAddress}
+                handleChangeText={(e) =>
+                  setform({ ...form, companyAddress: e })
+                }
+                isEditable={iconClicked}
+                icon={icons.map}
+                otherStyles="mt-7"
+              />
+              <CustomButton
+                title="Kaydet"
+                handlePress={handleActionPress}
+                containerStyles="bg-primary w-64 h-12 mt-7 items-center"
+                icon={icons.check}
+              />
+            </View>
+          )}
           <View className="justify-center items-center flex-row flex-1 mt-12">
-            <Image 
+            <Image
               source={icons.bag}
               className="w-12 h-12 mr-8"
-              style={{tintColor: "orange"}}
+              style={{ tintColor: "orange" }}
             />
             <Text className="text-xl text-black font-psemibold">
-          Siparişlerim
-          </Text>
-          </View>
-          <CustomButton 
-            title={ "Sipariş Görüntüle" }
-            handlePress={handleShowForm}
-            containerStyles="bg-primary w-64 h-12 mt-7 items-center"
-            icon={icons.down}
-          />
-        </View>
-        {showForm && (
-          <View className="w-full justify-center items-center mt-7">
-            <Text className="text-2xl font-psemibold">
-              Hesap Bilgileri {iconClicked ? "true" : "false"}
+              Siparişlerim
             </Text>
-            <Pressable onPress={handleIconPress}>
-              <Image
-                source={icons.change}
-                className="w-12 h-12 ml-8"
-                style={{ tintColor: iconClicked ? "orange" : "black" }}
-              />
-            </Pressable>
-            <FormField
-              title="İsim"
-              value={form.username}
-              handleChangeText={(e) => setform({ ...form, username: e })}
-              isEditable={iconClicked}
-              icon={icons.heart}
-              otherStyles="mt-7"
-            />
-            <FormField
-              title="Soyisim"
-              value={form.surname}
-              handleChangeText={(e) => setform({ ...form, surname: e })}
-              isEditable={iconClicked}
-              icon={icons.surname}
-              otherStyles="mt-7"
-            />
-            <FormField
-              title="Email"
-              value={form.email}
-              handleChangeText={(e) => setform({ ...form, email: e })}
-              isEditable={iconClicked}
-              icon={icons.mail}
-              otherStyles="mt-7"
-            />
-            <FormField
-              title="Şifre"
-              value={form.password}
-              handleChangeText={(e) => setform({ ...form, password: e })}
-              isEditable={iconClicked}
-              icon={icons.password}
-              otherStyles="mt-7"
-            />
-            <FormField
-              title="Telefon Numarası"
-              value={form.phoneNumber}
-              handleChangeText={(e) => setform({ ...form, phoneNumber: e })}
-              isEditable={!iconClicked}
-              icon={icons.smartPhone}
-              otherStyles="mt-7"
-            />
-            <FormField
-              title="Şirket İsmi"
-              value={form.companyName}
-              handleChangeText={(e) => setform({ ...form, companyName: e })}
-              isEditable={iconClicked}
-              icon={icons.home}
-              otherStyles="mt-7"
-            />
-            <FormField
-              title="Adres"
-              value={form.companyAddress}
-              handleChangeText={(e) => setform({ ...form, companyAddress: e })}
-              isEditable={iconClicked}
-              icon={icons.map}
-              otherStyles="mt-7"
-            />
-            <CustomButton
-              title="Kaydet"
-              handlePress={handleActionPress}
-              containerStyles="bg-primary w-64 h-12 mt-7 items-center"
-              icon={icons.check}
-            />
           </View>
-        )}
+          <CustomButton
+            title={showOrders ? "Siparişleri Gizle" : "Siparişleri Görüntüle"}
+            handlePress={handleShowOrders}
+            containerStyles="bg-primary w-64 h-12 mt-7 items-center"
+            icon={showOrders ?  icons.up : icons.down }
+          />
+          {showOrders && (
+            <View>
+              <Text> Merhaba </Text>
+            </View>
+          )}
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
