f3113c599c67de05c6a95747660749c9f3b1bc27
Furkan Deniz <mr0wido@protonmail.com>
2024-09-16 22:44:04 +0300

    new

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -41,6 +41,7 @@ const Profile = () => {
     <SafeAreaView className="h-full bg-white">
       <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
         <View className="flex-1 flex-col justify-center items-center mt-10">
+          {/* Hesap Bilgileri */}
           <View className="justify-center items-center flex-row flex-1">
             <Image
               source={icons.user}
@@ -54,97 +55,9 @@ const Profile = () => {
               icon={icons.down}
             />
           </View>
-          {showForms && (
-            <View className="w-full justify-center items-center mt-7">
-              <Text className="text-2xl font-psemibold">
-                Hesap Bilgileri {iconClicked ? "true" : "false"}
-              </Text>
-              <Pressable onPress={handleIconPress}>
-                <Image
-                  source={icons.change}
-                  className="w-12 h-12"
-                  style={{ tintColor: iconClicked ? "orange" : "black" }}
-                />
-              </Pressable>
-              <Text className=" mt-4 text-xl bg-slate-50 rounded-xl border-2 border-gray-300 text-black font-psemibold">
-                No: #5555
-              </Text>
-              <FormField
-                title="İsim"
-                value={form.username}
-                handleChangeText={(e) => setform({ ...form, username: e })}
-                isEditable={iconClicked}
-                icon={icons.heart}
-                otherStyles="mt-7"
-              />
-              <FormField
-                title="Soyisim"
-                value={form.surname}
-                handleChangeText={(e) => setform({ ...form, surname: e })}
-                isEditable={iconClicked}
-                icon={icons.surname}
-                otherStyles="mt-7"
-              />
-              <FormField
-                title="Email"
-                value={form.email}
-                handleChangeText={(e) => setform({ ...form, email: e })}
-                isEditable={iconClicked}
-                icon={icons.mail}
-                otherStyles="mt-7"
-              />
-              <FormField
-                title="Şifre"
-                value={form.password}
-                handleChangeText={(e) => setform({ ...form, password: e })}
-                isEditable={iconClicked}
-                icon={icons.password}
-                otherStyles="mt-7"
-              />
-              <FormField
-                title="Telefon Numarası"
-                value={form.phoneNumber}
-                handleChangeText={(e) => setform({ ...form, phoneNumber: e })}
-                isEditable={!iconClicked}
-                icon={icons.smartPhone}
-                otherStyles="mt-7"
-              />
-              <FormField
-                title="Şirket İsmi"
-                value={form.companyName}
-                handleChangeText={(e) => setform({ ...form, companyName: e })}
-                isEditable={iconClicked}
-                icon={icons.home}
-                otherStyles="mt-7"
-              />
-              <FormField
-                title="Adres"
-                value={form.companyAddress}
-                handleChangeText={(e) =>
-                  setform({ ...form, companyAddress: e })
-                }
-                isEditable={iconClicked}
-                icon={icons.map}
-                otherStyles="mt-7"
-              />
-              {iconClicked && (
-                <CustomButton
-                  title="Kaydet"
-                  handlePress={handleActionPress} // fonksiyonu ayarlanıcak
-                  containerStyles="bg-primary w-36 h-12 mt-7 items-center"
-                  icon={icons.check}
-                />
-              )}
-            </View>
-          )}
-          <CustomButton
-            title={showForms ? "Bilgileri Gizle" : "Bilgileri Görüntüle"}
-            handlePress={handleShowForms}
-            containerStyles="bg-primary w-64 h-12 -mt-14 items-center"
-            icon={showForms ? icons.up : icons.down}
-          />
-         
-          <View className="bg-bjustify-center items-center flex-row flex-1 mt-12">
+
+          {/* Siparişlerim */}
+          <View className="bg-gray-200 justify-center items-center flex-row flex-1 mt-12">
             <Image
               source={icons.bag}
               className="w-12 h-12 mr-8"
@@ -159,19 +72,15 @@ const Profile = () => {
           </View>
           {showOrders && (
             <View className="w-full justify-center items-center mt-7">
-              <Text> Merhaba </Text>
+              <Text>Merhaba</Text>
+              {/* Veritabanından siparişler buraya eklenecek */}
             </View>
-            // database den siparişler cekilecek
           )}
-          <CustomButton
-            title={showOrders ? "Siparişleri Gizle" : "Siparişleri Görüntüle"}
-            handlePress={handleShowOrders} // foknsiyomu ayarlanıcak
-            containerStyles="bg-primary w-64 h-12 -mt-14 items-center"
-            icon={showOrders ? icons.up : icons.down}
-          />
-          
+
+          {/* Çıkış Yap Butonu */}
           <CustomButton
             title="Çıkış Yap"
+            handlePress={handleLogout}
             containerStyles="bg-red-600 w-36 h-12 mt-7 mb-4 items-center"
             icon={icons.power}
           />
